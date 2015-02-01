dd63c7b2e983dbadbe84b15f59fc596f6988fc34
stefano.villa1976@gmail.com <stefano.villa1976@gmail.com>
2015-02-01 06:13:01 -0700

    cpufreq: yankactive: added new governor for latency-sensitive workloads
    Thanks a lot
    @neobuddy89
    @yank555-lu
    
    Signed-off-by: Zachariah <zkennedy87@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -418,6 +418,9 @@ extern struct cpufreq_governor cpufreq_gov_smartmax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX_EPS)
 extern struct cpufreq_governor cpufreq_gov_smartmax_eps;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax_eps)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
+extern struct cpufreq_governor cpufreq_gov_yankactive;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_yankactive)
 #endif
 
 
