4832f5162f99ccf29c8c3684757e890d6d23b735
jinqian <jinqian@google.com>
2015-04-13 14:12:47 -0700

    power: validate wakeup source before activating it.
    A rogue wakeup source not registered in wakeup_sources list is not visible
    from wakeup_sources_stats_show. Check if the wakeup source is registered
    properly by looking at the timer struct.
    
    Signed-off-by: jinqian <jinqian@google.com>
    Git-repo: https://android.googlesource.com/kernel/common.git
    Signed-off-by: Kaushal Kumar <kaushalk@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -498,6 +498,18 @@ static bool wakeup_source_blocker(struct wakeup_source *ws)
 	}
 
 	return false;
+/**
+ * wakeup_source_not_registered - validate the given wakeup source.
+ * @ws: Wakeup source to be validated.
+ */
+static bool wakeup_source_not_registered(struct wakeup_source *ws)
+{
+	/*
+	 * Use timer struct to check if the given source is initialized
+	 * by wakeup_source_add.
+	 */
+	return ws->timer.function != pm_wakeup_timer_fn ||
+		   ws->timer.data != (unsigned long)ws;
 }
 
 /*
@@ -539,6 +551,10 @@ static bool wakeup_source_blocker(struct wakeup_source *ws)
 static void wakeup_source_activate(struct wakeup_source *ws)
 {
 	unsigned int cec;
+
+	if (WARN(wakeup_source_not_registered(ws),
+			"unregistered wakeup source\n"))
+		return;
 
 	/*
 	 * active wakeup source should bring the system
