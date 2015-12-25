b1bc5c78837363155e17f1f9cab85c8c2a2baf1e
franciscofranco <franciscofranco.1990@gmail.com>
2015-12-25 17:54:33 +0000

    cpufreq: cpu-boost: export input_boost_enable to userspace
    Users should have the choice to run touchboost-less (even though it ruins performance)
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/cpu-boost.c

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -53,6 +53,7 @@ module_param(sync_threshold, uint, 0644);
 
 static unsigned int input_boost_enabled = 1;
 module_param(input_boost_enabled, uint, 0644);
+static bool suspended;
 
 static unsigned int input_boost_ms = 40;
 module_param(input_boost_ms, uint, 0644);
